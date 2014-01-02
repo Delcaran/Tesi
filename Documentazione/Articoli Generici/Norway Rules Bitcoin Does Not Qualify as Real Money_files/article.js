b5fb2f09894207dd51536660a8b2a17d226f5f15
Matteo Paoluzzi <duwath@gmail.com>
2014-01-02 16:42:20 +0100

    Aggiunta altra documentazione generica

--- Documentazione/Articoli Generici/Norway Rules Bitcoin Does Not Qualify as Real Money_files/article.js
+++
@@ -1,7 +0,0 @@
-var news_counter = function(c_what,a_id,r_id,c_id){
-	if(c_what && a_id){
-		var url = "/counter/article";
-		url += "?c_what="+c_what+"&a_id="+a_id+"&r_id="+r_id+"&c_id="+c_id+"&referer="+encodeURIComponent(document.referrer);
-		$.ajax({"url":url,cache: false});		
-	}
-}
