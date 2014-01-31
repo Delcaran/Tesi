6fe5f3064944f193988efadf3137e8ceb391af6b
Matteo Paoluzzi <duwath@gmail.com>
2014-01-31 17:51:57 +0100

    Attivato il glossario, cominciata la correzione

--- Testo/compile.sh
+++ Testo/compile.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 FILE="tesi"
+VIEWER="okular"
 latex -synctex=1 -interaction=nonstopmode ${FILE}.tex
 bibtex ${FILE}.aux
 makeindex ${FILE}.idx
-latex -synctex=1 -interaction=nonstopmode ${FILE}.tex
 pdflatex -synctex=1 -interaction=nonstopmode ${FILE}.tex
-evince ${FILE}.pdf
+makeglossaries ${FILE}
+pdflatex -synctex=1 -interaction=nonstopmode ${FILE}.tex
+pdflatex -synctex=1 -interaction=nonstopmode ${FILE}.tex
+#${VIEWER} ${FILE}.pdf
